5ef2f0e8f28b7f4d05ab51dd23258806fb269c88
Jon Chirinos <chirinosjon@gmail.com>
2022-01-15 20:01:04 -0600

    updated reset button and working on high score table

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,37 @@
 // Global Variables
 const startBtn = document.getElementById("start");
+const resetBtn = document.getElementById("reset");
 const questionDiv = document.getElementById("question");
 const answersDiv = document.getElementById("answers");
 const timerElement = document.getElementById("timer");
+const scoreTable = document.getElementById("scoreTable");
+// const resetQuiz = document.getElement("#reset");
 const questions = [
-    {
-        title: "What is David Blaine's first name?",
-        answers: ["David", "Chris", "Joe Exotic"],
-        correct: "David",
-    },
-    {
-        title: "Where Was David Blaine Born?",
-        answers: ["New Jersey", "New York", "California"],
-        correct: "New York",
-    },
-    {
-        title: "What is color is David Blaine's Hair?",
-        answers: ["Purple", "Black", "Brown"],
-        correct: "Brown",
-    },
-    {
-        title: "How old is David Blaine?",
-        answers: ["38", "48", "44"],
-        correct: "48",
-    },
-    {
-        title: "What is David Blaine Actual Last Name?",
-        answers: ["Perez", "White", "Blaine"],
-        correct: "White",
-    },
+    // {
+    //     title: "What is David Blaine's first name?",
+    //     answers: ["David", "Chris", "Joe Exotic"],
+    //     correct: "David",
+    // },
+    // {
+    //     title: "Where Was David Blaine Born?",
+    //     answers: ["New Jersey", "New York", "California"],
+    //     correct: "New York",
+    // },
+    // {
+    //     title: "What is color is David Blaine's Hair?",
+    //     answers: ["Purple", "Black", "Brown"],
+    //     correct: "Brown",
+    // },
+    // {
+    //     title: "How old is David Blaine?",
+    //     answers: ["38", "48", "44"],
+    //     correct: "48",
+    // },
+    // {
+    //     title: "What is David Blaine Actual Last Name?",
+    //     answers: ["Perez", "White", "Blaine"],
+    //     correct: "White",
+    // },
     {
         title: "Is David Blaine the Greatest Magician Alive?",
         answers: ["Yes", "No"],
@@ -36,14 +39,17 @@ const questions = [
     },
 ];
 const scoreBtn = document.getElementById("initials");
-
+// let shuffledQuestions, questionDiv;
 let score = 0;
 let questionIndex = 0;
 let timerCount = 20;
 let isWin = false;
-
+let initials = "";
 // Functions
 function startGame() {
+    onClick = startBtn.style.visibility = "hidden";
+    // shuffledQuestions = questionDiv.ariaSort(() => Math.random() - 0.5);
+    // questionIndex = 0;
     // clear out previous questions
     answersDiv.textContent = "";
     // Show first question with answers
@@ -57,6 +63,11 @@ function startGame() {
         answerBtn.onclick = answerClick;
         answersDiv.appendChild(answerBtn);
     });
+}
+
+function resetQuiz() {
+    // localStorage.clear();
+    location.reload();
 }
 // Answer click function
 function answerClick() {
@@ -86,8 +97,6 @@ function answerClick() {
         score = score - 2;
     }
 }
-
-// End quiz
 
 // set timer
 function startTimer() {
@@ -122,9 +131,31 @@ function endGame() {
     isWin = true;
 }
 // Save high score
-function displayScores() {
-    localStorage.getItem("playerScore");
-    localStorage.getItem("initials");
+
+function displayScores(event) {
+    event.preventDefault();
+
+    const highScores = JSON.parse(window.localStorage.getItem("playerScore", "initials")) || [];
+    console.log(scoreBtn);
+    // const highScore = document.querySelector(".scoreTable");
+    highScores
+        .map(() => {
+            return `<li class="highScores">${score} ${initials}</li>`;
+        })
+        .join("");
+    if (!timerCount) {
+        peep();
+    } else {
+        checkScore();
+    }
 }
+
+function clearScores() {
+    highScore.splice(0, highScore.length);
+    localStorage.setItem("highScores", JSON.stringify(highScore));
+    populateTable();
+}
+
 // Initialization- start
 startBtn.addEventListener("click", startTimer);
+resetBtn.addEventListener("click", resetQuiz);
